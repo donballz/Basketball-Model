c9793627fe3d3f4dc9466e0daed0afbfe245eed9
Donald Armstead <donald.armstead@gmail.com>
2016-03-31 21:26:18 -0400

    team tables loaded2

--- CONSTANTS.rb
+++ CONSTANTS.rb
@@ -5,4 +5,7 @@ GLOBAL['path'] = '/Users/donald/Google Drive/Basketball Model/Basketball-Model/'
 GLOBAL['yaml'] = '/Users/donald/Google Drive/Basketball Model/Basketball-Model/yaml_files/'
 GLOBAL['site'] = 'http://www.basketball-reference.com/teams/'
 GLOBAL['cstr'] = '<h2 data-mobile-header="" style="">'
+GLOBAL['srvr'] = 'Donalds-Mini.attlocal.net'
+GLOBAL['user'] = 'ruby'
+GLOBAL['pswd'] = 'Rubycon1$'
 File.open(File.join(__dir__, "CONSTANTS.yml"), 'w') { |f| f.write GLOBAL.to_yaml }
--- get_all_team_detail.rb
+++ get_all_team_detail.rb
@@ -1,93 +1,81 @@
 require 'mysql'
 require_relative 'team_detail.rb'
 
-# to do list:
-# write function to get NBA_TEAMS for master loop
-# feels like I should only connect once and pass the con object around instead of 
-#  connecting per function. significantly reduce the number of code lines
-
-def build_column_list(table_name)
-	# gets columns from information_schema and parses into string for insert query
-	# not strictly necessary. response to misread error message
-	begin
-		con = Mysql.new 'Donalds-Mini.attlocal.net', 'ruby', 'Rubycon1$'
-		rows = con.query "SELECT COLUMN_NAME AS COL FROM INFORMATION_SCHEMA.COLUMNS
- 						WHERE TABLE_SCHEMA = 'BBALL' AND TABLE_NAME = 'NBA_#{table_name}';"
-
-		build_str = ''
-		rows.each_hash { |row| build_str += "#{row['COL']}," }
+def build_col_type_list(con, table_name)
+	# gets column types from information_schema and parses into array for insert query
+	rows = con.query "SELECT DATA_TYPE AS TYPE FROM INFORMATION_SCHEMA.COLUMNS
+					WHERE TABLE_SCHEMA = 'BBALL' AND TABLE_NAME = 'NBA_#{table_name}';"
 	
-	rescue Mysql::Error => e
-		puts e.errno
-		puts e.error
-	
-	ensure
-		con.close if con
-	end
-	return build_str = build_str[0...-1]
+	col_types = []
+	rows.each_hash { |row| col_types.push(row['TYPE']) }
+	return col_types[2..-1]
 end
 
-def write_to_sql(tables, team, year)
+def write_to_sql(con, tables, team, year)
 	# writes data from ruby object to mysql server
-	begin
-		con = Mysql.new 'Donalds-Mini.attlocal.net', 'ruby', 'Rubycon1$'
-		con.query("USE bball;")
-		tables.each do |t|
-			unless ['Team Miscellaneous', 'Draft Rights'].include?(t['name'])
-				if t['name'] == 'Current Roster'
-					name = 'Roster'
-				else
-					name = t['name'].gsub(' ', '_')
-				end
-				col_list = build_column_list(name)
-				build_str = ''
-				t['data'].each do |row| 
-					build_str += "('#{team}', #{year}"
-					for i in 0...t['type'].length
-						if t['type'][i] == 'varchar(255)'
-							build_str += ", '#{row[i]}'" 
-						else
-							build_str += ", #{row[i].to_f}" 
-						end
-					end
-					build_str += '),'
-				end
-				build_str = build_str[0...-1]
-				puts "NBA_#{name}"
-				con.query("INSERT INTO NBA_#{name} (#{col_list}) VALUES #{build_str}")
+	tables.each do |t|
+		unless ['Team Miscellaneous', 'Draft Rights', 'Injury Report'].include?(t['name'])
+			if t['name'] == 'Current Roster'
+				name = 'Roster'
+			else
+				name = t['name'].gsub(' ', '_')
 			end
+			col_list = build_col_type_list(con, name)
+			build_str = ''
+			t['data'].each do |row| 
+				build_str += "('#{team}', #{year}"
+				row.delete_at(6) if name == 'Roster' and year == '2016'
+				for i in 0...col_list.length
+					if col_list[i] == 'varchar'
+						build_str += ", '#{row[i].to_s.gsub("'", "`")}'" 
+					else
+						build_str += ", #{row[i].to_f}" 
+					end
+				end
+				build_str += '),'
+			end
+			build_str = build_str[0...-1]
+			puts "Building #{team} #{year} NBA_#{name}"
+			#puts "INSERT INTO NBA_#{name} VALUES #{build_str}" if name == 'Roster' and year == '2016'
+			con.query("INSERT INTO NBA_#{name} VALUES #{build_str}")
 		end
-
-	rescue Mysql::Error => e
-		puts e.errno
-		puts e.error
-	
-	ensure
-		con.close if con
 	end
 end
 
 def run_all_data(con)
 	# gets team and year from NBA_TEAMS and writes to yaml files
-	con.query("USE bball")
+	# due to source site changing franchise abbreviation, there were some manual
+	# 	adjustments needed to get all teams, all years
+	# Run once and then it's not needed. Use yaml bridge to run the data
 	rows = con.query("SELECT ABBREV, YEAR_FROM, YEAR_TO FROM NBA_ACTIVE_TEAMS")
 	rows.each_hash do |row|
-		if row['ABBREV'] > 'OKC'
 		i = row['YEAR_TO'].to_i
 		while i >= row['YEAR_FROM'].to_i and i >= 2001
 			tables = parse_team_data(row['ABBREV'], i)
 			puts "#{row['ABBREV']} #{i} #{tables[0]['name']} #{tables[0]['type'].length}"
 			i -= 1
 		end
-		end
 	end
 end
 
-tables = YAML.load_file(File.join('/Users/donald/Google Drive/Basketball Model/Basketball-Model/yaml_files/', 'atl_2015.yml'))
-#write_to_sql(tables, 'ATL', 2015)
+def yaml_to_sql(con, path)
+	# gets file names and teams to pass to sql via write_to_sql method
+	rows = con.query("SELECT * FROM NBA_YAML_BRIDGE")
+	rows.each_hash do |row|
+		tables = YAML.load_file(File.join(path, row['FILE']))
+		write_to_sql(con, tables, row['TEAM'], row['YEAR'])
+	end
+	#tables = YAML.load_file(File.join(path, 'atl_2006.yml'))
+	#write_to_sql(con, tables, 'ATL', 2006)
+end
+
 begin
-	con = Mysql.new 'Donalds-Mini.attlocal.net', 'ruby', 'Rubycon1$'
-	run_all_data(con)
+	global = YAML.load_file(File.join(__dir__, 'CONSTANTS.yml'))
+	con = Mysql.new global['srvr'], global['user'], global['pswd']
+	con.query("USE bball")
+	
+	#run_all_data(con)
+	yaml_to_sql(con, global['yaml'])
 
 rescue Mysql::Error => e
 	puts e.errno
