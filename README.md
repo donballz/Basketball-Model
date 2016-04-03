4277083f0e2507a9d5e4d9de2f41940a0b0751a8
Donald Armstead <donald.armstead@gmail.com>
2016-04-03 17:44:12 -0400

    game detail partially yaml'd, need to understand timeout issues

--- README.md
+++ README.md
@@ -1,29 +1,55 @@
 # Basketball-Model
 
 Step 1 In dev: crawl Basketball Reference and get data for modeling. 
+
 Step 2 ?
+
 Step 3 PROFIT!!
 
+
+
 file list:
+
 CONSTANTS.rb - creates CONSTANTS.yml with needed global constants
+
 CONSTANTS.yml - created from CONSTANTS.rb. Stores constants
+
 README.md - this file with documentation
+
 StringFind.rb - Extends the String base class with a few helper functions to simplify code
+
 active_teams.sql - create table NBA_ACTIVE_TEAMS from NBA_TEAMS where defunct = 'N'
+
 atl_raw.txt - text of ATL 2016 website saved for testing purposes
+
 atl_tables.yml - parsed data of the test page saved for testing
+
 get_teams.rb - creates table NBA_TEAMS from http://www.basketball-reference.com/teams/
+
 			 - get_teams was the first file written and has significant learning curve
+
 mysql_connect_test.rb - used to learn the ruby connection to mysql
+
 team_detail_testing.rb - used in the build phase of team_detail.rb
+
 team_detail.rb - parses a page of single team for a single year into ruby Hash.
 
+
+
 drop_gen_tables.sql - drops all gen tables to restart during testing
+
 dump_raw.rb - creates files like atl_raw.txt
+
 GEN_TABLE_LIST.csv - List of team tables scraped from source
+
 gen_sql_strings.rb - creates strings for create gen table queries
+
 gen_table_build_test.sql - counts rows per table
+
 get_all_team_detail.rb - parses all the data; then uploads to sql
+
 atl_raw15.txt - text of ATL 2015 website saved for testing purposes, 15 tables differ from 16
+
 create_gen_tables.sql - builds empty gen tables. stable platform makes uploading easier
+
 create_yaml_bridge.sql - workaround for changing franchise abbrevs on source
--- get_all_game_detail.rb
+++ get_all_game_detail.rb
@@ -7,14 +7,17 @@ require_relative 'team_detail.rb'
 # play-by-play: http://www.basketball-reference.com/boxscores/pbp/200010310ATL.html
 # plus-minus: http://www.basketball-reference.com/boxscores/plus-minus/200010310ATL.html
 
+# need a way to validate a game has been loaded in three parts. 
+#   upload to sql server Y/N
+#   important for the long term as we begin to load games from the active season
+#   need to add a primary key 'game' to the REGULAR_SEASON and PLAYOFFS tables
 # basic and advanced stats in same "table" 
 #   different number of columns
 #   separated by team totals
+# plus-minus and pbp tables need significant post processsing
 # name of boxscore table should be in a column
-# need custom upload function
-# need custom plus-minus parser
-# need custom pbp parser.
-# FUCK.
+# need custom upload function for box score data
+# add these functions to test suite
 
 class Mysql
 	def each_game(sql_table)
@@ -26,29 +29,30 @@ class Mysql
 	end
 end
 
-def get_pbp_data(str, start, limit)
+def get_custom_data(str, row_starts, search, end_search, skip = 0)
 	# build array of arrays with data values
-	row_starts = get_start_pos(str, '<tr>', start, limit)
 	mydata = []
-	for i in 1..row_starts.length-2
+	for i in skip..row_starts.length-2
 		row = []
 		pos = row_starts[i]
 		col_cnt = 0
 		until pos == -1 or pos > row_starts[i + 1]
-			pos = str.find('<td', pos)
+			pos = str.find(search, pos)
+			if str[pos...str.find('>', pos)].find('width:') > -1
+				inner_pos = str.find('width:', pos)
+				ender_pos = str.index('px', inner_pos)
+				datum = str[inner_pos...ender_pos] + '/'
+			else
+				datum = ''
+			end
 			pos = str.find('>', pos)
-			end_pos = str.index('</td>', pos)
+			end_pos = str.index(end_search, pos)
 			unless pos == -1 or pos > row_starts[i + 1]
-				datum = clean_string(str[pos...end_pos])
-				if datum.to_i == 0 or col_cnt == 0
-					if datum == '&nbsp;'
-						row.push(nil) 
-					else
-						row.push(datum)
-					end
-				else
-					row.push(datum.to_f)
+				datum += clean_string(str[pos...end_pos])
+				until datum.find('&nbsp;') == -1
+					datum.slice!('&nbsp;')
 				end
+				row.push(datum)
 			end
 			col_cnt += 1
 		end
@@ -65,10 +69,31 @@ def get_pbp_tables(str, starts)
 		pos = starts[i]
 		end_pos = str.index('</h2>', pos)
 		table['name'] = str[pos...end_pos]
-		table['data'] = get_pbp_data(str, starts[i], starts[i + 1])
+		row_starts = get_start_pos(str, '<tr>', starts[i], starts[i + 1])
+		table['data'] = get_custom_data(str, row_starts, '<td', '</td>', 1)
 		tables.push(table)
 	end
 	return tables
+end
+
+def get_pm_tables(str, starts)
+	# build master array with tables from one page as ruby objects
+	tables = []
+	for i in 0..starts.length-2
+		table = {}
+		table['name'] = 'Plus-Minus'
+		row_starts = get_start_pos(str, '<div>', starts[i], starts[i + 1])
+		table['data'] = get_custom_data(str, row_starts, '<div class="', '</div>')
+		tables.push(table)
+	end
+	return tables
+end
+
+def yaml_files(tables, path, file)
+	# quick helper to send tables to yaml
+	File.open(File.join(path, file), 'w') do |f| 
+		f.write tables.to_yaml
+	end
 end
 
 begin
@@ -76,30 +101,34 @@ begin
 	con = Mysql.new global['srvr'], global['user'], global['pswd']
 	con.query("USE bball")
 	
-	['NBA_REGULAR_SEASON'].each do |sqlt|
+	['NBA_REGULAR_SEASON', 'NBA_PLAYOFFS'].each do |sqlt|
 		con.each_game(sqlt) do |game| 
 			unless game == ''
+				puts "processing #{game}"
 				# get box scores. complete.
-				#page_raw = URI.parse("#{global['site']}boxscores/#{game}.html").read
-				#table_starts = get_start_pos(page_raw, global['boxt'], 0, page_raw.length)
-				#tables = get_tables(page_raw, table_starts)
-				#sleep(3)
+				page_raw = URI.parse("#{global['site']}boxscores/#{game}.html").read
+				table_starts = get_start_pos(page_raw, global['boxt'], 0, page_raw.length)
+				tables = get_tables(page_raw, table_starts)
+				yaml_files(tables, global['yaml'], "zbs_#{game}.yml")
+				sleep(3)
 				
-				# get play-by-play. in dev.
+				# get play-by-play. complete.
 				page_raw = URI.parse("#{global['site']}boxscores/pbp/#{game}.html").read
 				table_starts = get_start_pos(page_raw, global['cstr'], 0, page_raw.length)
-				puts get_pbp_tables(page_raw, table_starts)
-				#sleep(3)
+				tables = get_pbp_tables(page_raw, table_starts)
+				yaml_files(tables, global['yaml'], "zpbp_#{game}.yml")
+				sleep(3)
 				
-				# get plus minus. not started.
-				#page_raw = URI.parse("#{global['site']}boxscores/plus-minus/#{game}.html").read
-				
-				#sleep(3)
+				# get plus minus. complete.
+				pm = '<div style="width:1005px;'
+				page_raw = URI.parse("#{global['site']}boxscores/plus-minus/#{game}.html").read
+				table_starts = get_start_pos(page_raw, pm, 0, page_raw.length)
+				tables = get_pm_tables(page_raw, table_starts)
+				yaml_files(tables, global['yaml'], "zpm_#{game}.yml")
+				sleep(3)
 			end
-			break
 		end
 	end
-	#con.each_game('NBA_PLAYOFFS')
 
 rescue Mysql::Error => e
 	puts e.errno
