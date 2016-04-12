fe0a3d3b43326cd1ca727f1b8012875a3e5b5b0b
Donald Armstead <donald.armstead@gmail.com>
2016-04-11 20:14:29 -0400

    plus-minus process complete

--- game_detail_upload.rb
+++ game_detail_upload.rb
@@ -1,4 +1,5 @@
 require 'yaml'
+require_relative 'StringFind.rb'
 
 def bs_post(game, path)
 	# post processing for boxscore game data. last row has totals; limited out
@@ -56,19 +57,76 @@ def pm_first(tables)
 	return output
 end
 
+def min_per(per)
+	# number of minutes in a period
+	return 12 if per < 5
+	return 5
+end
+
+def pm_second(game, table)
+	# converts once-processed pm file into final format for upload
+	output, per_lens = [], []
+	for ind in 0...table.length
+		if ind == 0
+			team = table[0][0]
+			table[ind].each { |per| per_lens.push(per[0...per.find('/')].to_f) }
+			per_lens = per_lens[1..-1]
+		else
+			tot_rem = 0
+			per_lens.each { |d| tot_rem += d}
+			per_lens_inst = per_lens.dup
+			player = table[ind][0]
+			player = player[0...player.index('(') - 1]
+			for jnd in 1...table[ind].length
+				slash = table[ind][jnd].index('/')
+				dur = table[ind][jnd][0...slash].to_f
+				pm = table[ind][jnd][slash + 1..-1]
+				if pm == ""
+					status = 'off'
+				else
+					status = 'on'
+				end
+				while dur > 0 and tot_rem > 0 do
+					for per in 1..per_lens_inst.length
+						per_rem = per_lens_inst[per - 1]
+						#puts "#{per}, #{dur}, #{per_lens_inst.inspect}"
+						if per_rem > 0
+							if dur > per_rem
+								time = per_rem / per_lens[per - 1] * min_per(per)
+								dur -= per_rem
+								tot_rem -= per_rem
+								per_lens_inst[per - 1] = 0
+							else
+								time = dur / per_lens[per - 1] * min_per(per)
+								per_lens_inst[per - 1] -= dur
+								tot_rem -= dur
+								dur = 0
+							end
+							#puts "out"
+							output.push([game, team, player, per, time, status, pm])
+							break
+						end
+					end
+				end
+			end
+		end
+	end
+	return output
+end
+
 def pm_post(game, path)
 	# post processing for plus-minus data. hold onto your butts.
 	tables = YAML.load_file(File.join(path, "zpm_#{game}.yml"))
 	pm_tab = pm_first(tables)
 	away = pm_tab[0...pm_tab.length / 2]
 	home = pm_tab[pm_tab.length / 2...-1]
-	p away
-	puts
-	p home
+	pm_second(game, away)
+	#puts
+	#p home
 end
 
 global = YAML.load_file(File.join(__dir__, 'CONSTANTS.yml'))
 basic, advanced = bs_post('200203070SEA', global['yaml'])
 pbp =  pbp_post('200203070SEA', global['yaml'])
 pm = pm_post('200203070SEA', global['yaml'])
-#puts pm.length
+pm.each { |row| p row}
