227e0190a3899bd2554ad8d77df04e07557d0f61
Donald Armstead <donald.armstead@gmail.com>
2016-05-21 00:19:30 -0400

    pick back up status check

--- active_teams.sql
+++
@@ -1,9 +0,0 @@
-use bball;
-drop table if exists NBA_ACTIVE_TEAMS;
-create table if not exists NBA_ACTIVE_TEAMS as
-select *
-from nba_teams
-where defunct = 'N'
-;
-
-SELECT ABBREV, YEAR_FROM, YEAR_TO FROM NBA_ACTIVE_TEAMS;
