edf4b3c9f3d1d185ec5a95444abd044a4ff19e6e
Donald Armstead <donald.armstead@gmail.com>
2016-05-24 21:59:58 -0400

    feat: failed attempt to get self-email sending

---
+++ send_email_test.rb
@@ -0,0 +1,18 @@
+require 'mail'
+
+Mail.defaults do
+  delivery_method :smtp, :address    => "smtp-relay.gmail.com",
+                          :port       => 465,
+                          :user_name  => 'adoubledot@gmail.com',
+                          :password   => 'curtis3078',
+                          :enable_ssl => true
+end
+
+mail = Mail.new do
+  from    'adoubledot@gmail.com'
+  to      'donald.armstead@gmail.com'
+  subject 'Code completed'
+  body    'Now get back to work.'
+end
+
+mail.deliver
