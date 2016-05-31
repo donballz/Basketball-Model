541f969712e4f8f530ed8babaefd5cf75c1891f1
Donald Armstead <donald.armstead@gmail.com>
2016-05-31 01:31:03 -0400

    feat: add common functions to single file. borrowed from Ruby Port

---
+++ common_funcs.rb
@@ -0,0 +1,26 @@
+require 'net/http'
+require 'uri'
+require 'yaml'
+require_relative 'CONSTANTS.rb'
+
+def write(obj, fname)
+	# writes any object to supplied filename. naming conflict with rT class func
+	File.open(PATH + "#{fname}.yml", 'w') { |f| f.write obj.to_yaml }
+end
+
+def read(fname, suffix='yml')
+	# reads yaml file to object
+	return YAML.load_file(PATH + "#{fname}.#{suffix}")
+end
+
+def get_page(url)
+	# fetches webpage as flat string
+	return Net::HTTP.get(URI.parse(url))
+end
+
+def annual_hash
+	# return hash of blank hashes by year
+	mh = {}
+	(2001..2016).to_a.each { |y| mh[y] = Hash.new(0) }
+	return mh
+end
